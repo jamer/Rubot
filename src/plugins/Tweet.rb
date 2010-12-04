c77300c80d8b8117922a8abf55ae8fae2f9ad275
Savetheinternet <savetheinternet@n0v4.com>
2010-12-04 22:08:11 +1100

    Added a bunch of new commands.

--- src/plugins/Tweet.rb
+++ src/plugins/Tweet.rb
@@ -8,7 +8,7 @@ require 'time'
 
 class Tweet < RubotPlugin
 	def initialize
-		@accounts = {"wikileaks" => 0, "wired" => 0}
+		@accounts = {:wikileaks => 0, :wired => 0}
 		@index = 0
 		@needs_to_be_said = []
 		
@@ -41,7 +41,7 @@ class Tweet < RubotPlugin
 	
 	def do_tweets
 		username = @accounts.keys[@index]
-		@accounts[username] = check_new_tweets(username, @accounts[username])
+		@accounts[username] = check_new_tweets(username.to_s, @accounts[username])
 		
 		@index+=1
 		if @index >= @accounts.count then
@@ -49,20 +49,54 @@ class Tweet < RubotPlugin
 		end
 	end
 	
-	def check_new_tweets(username, since_id)
+	def announce_tweets(tweets)
+		tweets.each do |tweet|
+			on_new_twitter_message @coder.decode(tweet['from_user']), @coder.decode(tweet['text'].split("\n").join " ")
+		end
+	end
+	
+	def search_tweets(query)
+		uri = "/search.json?lang=en&q=#{query}&rpp=5"
+		
 		Net::HTTP.new("search.twitter.com", 80).start do |http|
-			res = http.get "/search.json?lang=en&from=#{username}&since_id=#{since_id}", @h
+			res = http.get uri, @h
+			timeline = JSON.parse res.body
+			if not timeline.include? 'results' then
+				return "\002Error\002: [#{uri}] #{timeline.to_s}"
+			end
+			
+			if timeline['results'].empty? then
+				return "No results."
+			end
+			announce_tweets timeline['results'].reverse
+		end
+		nil
+	end
+	
+	def check_new_tweets(username, since_id)
+		if since_id < 0 then
+			# Latest
+			uri = "/search.json?lang=en&from=#{username}&rpp=1"
+		else
+			# Tweets after given ID
+			uri = "/search.json?lang=en&from=#{username}&since_id=#{since_id}"
+		end
+		
+		Net::HTTP.new("search.twitter.com", 80).start do |http|
+			res = http.get uri, @h
 			
 			timeline = JSON.parse res.body
 			
 			# Show tweets in reverse order. Oldest first.
+			if not timeline.include? 'results' then
+				@needs_to_be_said.push "\002Error\002: [#{uri}] #{timeline.to_s}"
+				return since_id
+			end
 			tweets = timeline['results'].reverse
 			
 			# Don't announce first tweets
-			if since_id > 0 then
-				tweets.each do |tweet|
-					on_new_twitter_message @coder.decode(tweet['from_user']), @coder.decode(tweet['text'].split("\n").join " ")
-				end
+			if since_id != 0 then
+				announce_tweets tweets
 			end
 			
 			if tweets.empty? then
@@ -76,11 +110,27 @@ class Tweet < RubotPlugin
 	
 	def privmsg(user, reply_to, message)
 		if message =~ /^!follow ([A-Za-z0-9]+)$/ then
-			@accounts[$1] = 0
-			say reply_to, "Following \002#{$1}\002."
-		end
-		if message =~ /^!following$/ then
+			username = $1.to_sym
+			if @accounts.include? username then
+				say reply_to, "I'm already following \002#{$1}\002."
+			else
+				@accounts[username] = 0
+				say reply_to, "Following \002#{$1}\002."
+			end
+		elsif message =~ /^!unfollow ([A-Za-z0-9]+)$/ then
+			username = $1.to_sym
+			if @accounts.include? username then
+				@accounts.delete username
+				say reply_to, "Stopped following \002#{$1}\002."
+			else
+				say reply_to, "I'm not following \002#{$1}\002!"
+			end
+		elsif message =~ /^!following$/ then
 			say reply_to, "Following: \002#{@accounts.keys.join ", "}\002."
+		elsif message =~ /^!latest ([A-Za-z0-9]+)$/ then
+			check_new_tweets $1, -1
+		elsif message =~ /^!search (.+)$/ then
+			say reply_to, search_tweets(URI.escape $1)
 		end
 	end
 	
