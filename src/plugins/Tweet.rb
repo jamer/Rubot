18449cb4e0c974aa36c881c6eacf9892f1357e8f
Savetheinternet <savetheinternet@n0v4.com>
2010-12-04 19:37:27 +1100

    Made a working plugin

--- src/plugins/Tweet.rb
+++ src/plugins/Tweet.rb
@@ -2,37 +2,81 @@ require 'json'
 require 'net/http'
 require 'uri'
 require 'htmlentities'
+require 'time'
 
 # http://search.twitter.com/search.json?lang=en&from={user}
 
 class Tweet < RubotPlugin
-
+	def initialize
+		@accounts = {"wikileaks" => 0, "wired" => 0}
+		@index = 0
+		@needs_to_be_said = []
 		
-	def check_new_tweets(username)
-		Net::HTTP.new("search.twitter.com", 80).start do |http|
-			res = http.get "/search.json?lang=en&from=#{username}"
-			p res
-			
-			timeline = JSON.parse res.body
-			timeline['results'].each do |tweet|
-				on_new_twitter_message @coder.decode(tweet['from_user']), @coder.decode(tweet['text'])
+		@coder = HTMLEntities.new
+		
+		Thread.new {
+			while true
+				do_tweets
+				sleep 1
 			end
-			
-			return res.body
+		}
+		
+		Thread.new {
+			while true
+				@needs_to_be_said.each do |line|
+					@client.channels.keys.each { |channel| say channel, line }
+					@needs_to_be_said.delete line
+					sleep 0.3
+				end
+				sleep 1
+			end
+		}
+	end
+	
+	def do_tweets
+		username = @accounts.keys[@index]
+		@accounts[username] = check_new_tweets(username, @accounts[username])
+		
+		@index+=1
+		if @index >= @accounts.count then
+			@index = 0
 		end
 	end
 	
-	def initialize
-		@coder = HTMLEntities.new
+	def check_new_tweets(username, since_id)
+		Net::HTTP.new("search.twitter.com", 80).start do |http|
+			res = http.get "/search.json?lang=en&from=#{username}&since_id=#{since_id}"
+			
+			timeline = JSON.parse res.body
+			
+			# Show tweets in reverse order. Oldest first.
+			tweets = timeline['results'].reverse
+			
+			# Don't announce first tweets
+			if since_id > 0 then
+				tweets.each do |tweet|
+					on_new_twitter_message @coder.decode(tweet['from_user']), @coder.decode(tweet['text'].split("\n").join " ")
+				end
+			end
+			
+			if tweets.empty? then
+				return since_id
+			else
+				return tweets.reverse[0]['id']
+			end
+			
+		end
 	end
-
+	
 	def privmsg(user, reply_to, message)
-		check_new_tweets("wikileaks")
-		say reply_to, "I heard you, #{user.nick}"
+		if message =~ /!follow ([A-Za-z0-9]+)/ then
+			@accounts[$1] = 0
+			say reply_to, "Following \002#{$1}\002."
+		end
 	end
 	
 	def on_new_twitter_message(from, message)
-		@client.channels.keys.each { |channel| say channel, "#{from} says #{message}" }
+		@needs_to_be_said.push "\002[#{from}]\002 #{message}"
 	end
 end
 
