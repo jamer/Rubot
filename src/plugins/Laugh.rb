4233d7b9275b8ca493af0f29ac89170c883dd0aa
Paul Merrill <napalminc@gmail.com>
2010-12-17 01:08:44 -0800

    Commented laugh module.

--- src/plugins/Laugh.rb
+++ src/plugins/Laugh.rb
@@ -1,5 +1,11 @@
 
 class Laugh < RubotPlugin
+	# Laugh at command.
+	#
+	# Says "LOL" when someone types "laugh 1"
+	# Says "LOLO-" when someone types "laugh 1.5"
+	# Says "LOLOL" when someone types "laugh 2"
+	# Etc.
 
 	@@privmsg_actions = {
 		/laugh\s*(\d+\.?\d?)/i => :laugh,
@@ -12,13 +18,14 @@ class Laugh < RubotPlugin
 		return al.parse(message, [source])
 	end
 
+	# Say "WOLOL" about 10% of the time.
 	def first_char
 		return rand(10) == 1 ? "W" : "L"
 	end
 
 	# Laugh "times" times. If times is a partial decimal, add an extra "O-"
-	# to the end of it. Maximum length of laugh is 25 times (plus the O- if
-	# we're a decimal).
+	# to the end of it. Maximum number of times 25 (plus the O- if we're a
+	# decimal).
 	def laugh source, times
 		times = times.to_f
 		decimal = times.floor != times
