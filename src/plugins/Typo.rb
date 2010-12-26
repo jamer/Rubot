dd9a38fe250a4669dd709009e7cc4face89bc27b
Paul Merrill <napalminc@gmail.com>
2010-12-26 05:05:12 -0800

    Removed commented lines in Typo.

--- src/plugins/Typo.rb
+++ src/plugins/Typo.rb
@@ -7,16 +7,11 @@ class Typo < RubotPlugin
 			return unless @@last_msg[user.nick]
 			orig = $1
 			new = $2
-			msg = @@last_msg[user.nick]
-			msg.sub! /#{orig}/, new
+			msg = @@last_msg[user.nick].sub! /#{orig}/, new
 			say source, "#{user.nick} meant to say: #{msg}"
-			@@last_msg[user.nick] = msg
 		else
 			@@last_msg[user.nick] = message
 		end
-#		if message.match /^merry.*christmas/i
-#			say source, "Merry Christmas, #{user.nick}!"
-#		end
 	end
 
 end
