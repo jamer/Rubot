c40d5285420f49716ddf3006e1492bbac3a5239a
Paul Merrill <napalminc@gmail.com>
2011-01-11 12:54:56 -0800

    Typo remembers multiple previous lines.

--- src/plugins/Typo.rb
+++ src/plugins/Typo.rb
@@ -1,16 +1,33 @@
 
 class Typo < RubotPlugin
+	@@remembered = 10
 	@@last_msg = {}
 
-	def privmsg(user, source, message)
-		if message.match /^s\/(.*)\/(.*)\/?/
-			return unless @@last_msg[user.nick]
+	def user_said nick, message
+		unless @@last_msg[nick]
+			@@last_msg[nick] = Array.new
+		end
+
+		@@last_msg[nick].push message
+		if @@last_msg[nick].length > @@remembered
+			@@last_msg[nick].shift
+		end
+	end
+
+	def privmsg user, source, message
+		nick = user.nick
+		if message.match /^s\/(.+)\/(.+)/
+			return unless @@last_msg[nick]
 			orig = $1
-			new = $2
-			msg = @@last_msg[user.nick].sub! /#{orig}/, new
-			say source, "#{user.nick} meant to say: #{msg}"
+			cor = $2
+			cor.chop! if cor[-1..-1] == "/"
+			found = @@last_msg[nick].grep(/#{orig}/).last
+			return unless found
+			corrected = found.sub /#{orig}/i, cor
+			say source, "#{nick} meant to say: #{corrected}"
+			user_said nick, corrected
 		else
-			@@last_msg[user.nick] = message
+			user_said nick, message
 		end
 	end
 
