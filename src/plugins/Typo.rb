db5848f71f77b2a8c8da2f8712a708d44a0a0bd8
Paul Merrill <napalminc@gmail.com>
2011-01-12 17:37:07 -0800

    Can correct what other people say.

--- src/plugins/Typo.rb
+++ src/plugins/Typo.rb
@@ -14,19 +14,30 @@ class Typo < RubotPlugin
 		end
 	end
 
+	def replace source, nick, orig, cor
+		return unless @@last_msg[nick]
+		found = @@last_msg[nick].grep(/#{orig}/).last
+		return unless found
+		corrected = found.sub /#{orig}/i, cor
+		say source, "#{nick} meant to say: #{corrected}"
+		user_said nick, corrected
+	end
+
 	def privmsg user, source, message
-		nick = user.nick
 		if message.match /^s\/(.+)\/(.+)/
-			return unless @@last_msg[nick]
+			nick = user.nick
 			orig = $1
 			cor = $2
 			cor.chop! if cor[-1..-1] == "/"
-			found = @@last_msg[nick].grep(/#{orig}/).last
-			return unless found
-			corrected = found.sub /#{orig}/i, cor
-			say source, "#{nick} meant to say: #{corrected}"
-			user_said nick, corrected
+			replace source, nick, orig, new
+		elsif message.match /^(.+?)\/(.+)\/(.+)/
+			nick = $1
+			orig = $2
+			cor = $3
+			cor.chop! if cor[-1..-1] == "/"
+			replace source, nick, orig, cor
 		else
+			nick = user.nick
 			user_said nick, message
 		end
 	end
