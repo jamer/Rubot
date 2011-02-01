fa244a6d8b1b3f2afb56c9f3960f583e3537e64f
Paul Merrill <napalminc@gmail.com>
2011-01-31 19:11:55 -0800

    Added 'Rage' plugin.

---
+++ src/plugins/Rage.rb
@@ -0,0 +1,26 @@
+
+class Rage < RubotPlugin
+	@@privmsg_actions = {
+		/the game/i => :rage,
+	}
+
+	def initialize
+		@cnt = 0
+	end
+
+	def privmsg(user, source, message)
+		al = ActionList.new @@privmsg_actions, self
+		return al.parse(message, [user.nick, source])
+	end
+
+	def rage nick, source
+		@cnt += 1
+		if @cnt == 3
+			say source, "explodes at #{nick}! >:(", :action
+			@cnt = 0
+		else
+			say source, "Grr... #{nick}. :("
+		end
+	end
+end
+
