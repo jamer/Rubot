be2f7bca0cd3aa7b8eafc5e60c973033b7155c16
Paul Merrill <napalminc@gmail.com>
2011-09-26 23:27:36 -0700

    moved Rejoin to its own plugin

--- plugins/General.rb
+++ plugins/General.rb
@@ -6,8 +6,6 @@ class General < RubotPlugin
 		/^:leave (.+)/i => :part,
 		/^:part$/i => :part_this,
 		/^:leave$/i => :part_this,
-		/^:rejoin (.+)/i => :rejoin,
-		/^:norejoin (.+)/i => :norejoin,
 	}
 
 	def initialize
@@ -36,43 +34,6 @@ class General < RubotPlugin
 
 	def part_this(user, source)
 		@client.part(source)
-	end
-
-	def rejoin(user, source, channels)
-		when_identified(user, proc {
-			split_channels(channels).each do |channel|
-				if user.privilege(channel) < ChannelOp
-					say(source, "Must be channel op.")
-					next
-				end
-				next unless @client.channels.include?(channel)
-				@client.channels[channel].rejoin = true
-				say(source, "rejoin #{channel} ON")
-			end
-		})
-	end
-
-	def norejoin(user, source, channels)
-		when_identified(user, proc {
-			split_channels(channels).each do |channel|
-				if user.privilege(channel) < ChannelOp
-					say(source, "Must be channel op.")
-					next
-				end
-				next unless @client.channels.include?(channel)
-				@client.channels[channel].rejoin = false
-				say(source, "rejoin #{channel} OFF")
-			end
-		})
-	end
-
-	def split_channels(channels)
-		channels = channels.to_s if channels.is_a?(Integer)
-		return channels.split(',').map { |channel|
-			channel.strip!
-			channel = '#' + channel if channel.size > 0 and channel[0,1] != '#'
-			(channel.empty?) ? nil : channel
-		}.compact
 	end
 end
 
---
+++ plugins/Rejoin.rb
@@ -0,0 +1,54 @@
+# Adds commands to control automatic rejoining of channels. There is no
+# persistent storage in this plugin, so channels will have to be readded on
+# each run.
+class Rejoin < RubotPlugin
+	@@actions = {
+		/^:rejoin (.+)/i => :rejoin,
+		/^:norejoin (.+)/i => :norejoin,
+	}
+
+	def initialize
+		super
+	end
+
+	def on_privmsg(user, source, msg)
+		RegexJump::jump(@@actions, self, msg, [user, source])
+	end
+
+	def on_join(user, channel)
+		# TODO: Persist channel auto-rejoin status.
+	end
+
+	# Enables automatic rejoining for the specified channels.
+	def rejoin(user, source, channels)
+		on_identification(user) do
+			split_channels(channels).each do |channel|
+				if user.privilege(channel) < ChannelOp
+					say(source, "You must be at least a channel operator on #{channel}.")
+				elsif not @client.channels.include?(channel)
+					say(source, "I'm not in #{channel}.")
+				else
+					@client.channels[channel].rejoin = true
+					say(source, "rejoin #{channel} ON")
+				end
+			end
+		end
+	end
+
+	# Disables automatic rejoining for the specified channels.
+	def norejoin(user, source, channels)
+		on_identification(user) do
+			split_channels(channels).each do |channel|
+				if user.privilege(channel) < ChannelOp
+					say(source, "You must be at least a channel operator on #{channel}.")
+				elsif not @client.channels.include?(channel)
+					say(source, "I'm not in #{channel}.")
+				else
+					@client.channels[channel].rejoin = false
+					say(source, "rejoin #{channel} OFF")
+				end
+			end
+		end
+	end
+end
+
